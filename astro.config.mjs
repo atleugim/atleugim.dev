d7a5dd405e01c1a329714834a6cad3287c3dfd3f
Miguel Vega <vegaleugim@gmail.com>
2024-12-11 22:00:46 -0500

    feat: add Vercel adapter for deployment; update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-// @ts-check
-import metaTags from "astro-meta-tags";
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   site: "https://atleugim.dev",
-  integrations: [tailwind(), sitemap(), metaTags()]
+  integrations: [tailwind(), sitemap()]
 });
