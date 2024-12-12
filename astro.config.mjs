695c96a250cbd8a8f39dc25e00fff7b8ade5c383
Miguel Vega <vegaleugim@gmail.com>
2024-12-11 21:16:21 -0500

    feat: add sitemap and meta tags integration; update site metadata and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+
+import metaTags from 'astro-meta-tags';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://atleugim.dev',
+  integrations: [tailwind(), sitemap(), metaTags()]
 });
