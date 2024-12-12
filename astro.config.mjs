af1de3c7c9a1de6eea1d8d5db700ed5f27e6389d
Miguel Vega <vegaleugim@gmail.com>
2024-12-11 21:17:16 -0500

    fix: standardize import statements and formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-
-import metaTags from 'astro-meta-tags';
+import metaTags from "astro-meta-tags";
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atleugim.dev',
+  site: "https://atleugim.dev",
   integrations: [tailwind(), sitemap(), metaTags()]
 });
