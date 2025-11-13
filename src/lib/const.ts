c53fdb35d3455b0268a8a4a7ea8419dd41fbfcb1
Miguel Vega <vegaleugim@gmail.com>
2025-11-13 12:00:02 -0500

    feat: add resume download link and update constants

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -3,6 +3,8 @@ export const EMAIL_ADDRESS = import.meta.env.EMAIL_ADDRESS;
 export const GH_URL = "https://github.com/atleugim";
 export const LI_URL = "https://linkedin.com/in/atleugim";
 export const EMAIL_URL = `mailto:${EMAIL_ADDRESS}`;
+export const RESUME_URL =
+  "https://raw.githubusercontent.com/atleugim/static/main/resume.pdf";
 
 export const SPOTIFY_TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 export const SPOTIFY_NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
